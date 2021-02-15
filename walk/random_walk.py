6d0e563686234df4001c8263149bb39c1fdc6c5f
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-02-15 12:11:56 -0300

    Colors can be chosen randomly

--- walk/random_walk.py
+++ walk/random_walk.py
@@ -1,22 +1,27 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-jackie = Turtle()
-jackie.shape("turtle")
-jackie.color("dark slate gray")
+jackie = t.Turtle()
+t.colormode(255)
 
-colors = ["orange red", "red", "tomato", "light coral", "pink", "brown", "purple", "firebrick", "maroon"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 direction = [0, 90, 180, 270]
 jackie.pensize(10)
 jackie.speed("fastest")
 
-def draw_path():
-    jackie.pencolor(random.choice(colors))
+
+for line in range(200):
+    jackie.pencolor(random_color())
     jackie.forward(25)
     jackie.setheading(random.choice(direction))
 
-for line in range(200):
-    draw_path()
 
 screen = Screen()
 screen.exitonclick()
